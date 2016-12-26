c2f17675ce569ae56c329d5fc761a5e7440cc228
User1 <xumichael94@yahoo.com>
2016-12-26 12:51:59 -0600

    Added Comment Support

--- app/src/main/java/com/example/sriramhariharan/cyfallsapp2016/Assignment.java
+++ app/src/main/java/com/example/sriramhariharan/cyfallsapp2016/Assignment.java
@@ -4,14 +4,16 @@ package com.example.sriramhariharan.cyfallsapp2016;
  * Created by SriramHariharan on 12/22/16.
  */
 
-public class Assignment{
+class Assignment {
+
     String duedate;
     String name;
     String type;
     String grade;
     String totalscore;
+    String comment = "";
 
-    public Assignment(String d,String n,String t,String g,String ts){
+    public Assignment(String d, String n, String t, String g, String ts, String comment) {
         duedate = d;
         name = n;
         type = t;
@@ -19,43 +21,14 @@ public class Assignment{
         totalscore = ts;
     }
 
-    public String getName() {
-        return name;
-    }
-
-    public String getDuedate() {
-        return duedate;
-    }
-
-    public String getGrade() {
-        return grade;
-    }
-
-    public String getTotalscore() {
-        return totalscore;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public String toString(){
-        String ret = "{"+duedate+"}"+
-                "{"+name+"}"+
-                "{"+type+"}"+
-                "{"+grade+"}"+
-                "{"+totalscore+"}";
+    public String toString() {
+        String ret = "{" + duedate + "}"
+                + "{" + name + "}"
+                + "{" + type + "}"
+                + "{" + grade + "}"
+                + "{" + totalscore + "}"
+                + "{" + comment + "}";
         return ret;
     }
 
-    @Override
-    public boolean equals(Object obj) {
-        Assignment assignment = (Assignment)obj;
-        if(duedate.equals(assignment.duedate) && name.equals(assignment.name) && type.equals(assignment.type) && grade.equals(assignment.grade) && totalscore.equals(assignment.totalscore)){
-            return true;
-        }
-        else{
-            return false;
-        }
-    }
 }
--- app/src/main/java/com/example/sriramhariharan/cyfallsapp2016/ClssPkg.java
+++ app/src/main/java/com/example/sriramhariharan/cyfallsapp2016/ClssPkg.java
@@ -15,95 +15,89 @@ import java.io.Serializable;
 import java.net.Socket;
 import java.util.ArrayList;
 import java.util.Scanner;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 /**
  *
  * @author User1
  */
 public class ClssPkg implements Serializable{
+
+    String lunch;
     int[] abs = new int[8];
     String absString = "";
     String name;
-    String lunch;
     ArrayList<Course> classes = new ArrayList<Course>();
 
-    public ClssPkg(String n){
+    public ClssPkg(String n) {
         name = n;
     }
 
-    public Course get(String n){
-        for(Course c : classes){
-            if((c.coursenum+" "+c.name).equals(n))return c;
+    public Course get(String n) {
+        for (Course c : classes) {
+            if ((c.coursenum + " " + c.name).equals(n)) {
+                return c;
+            }
         }
         return null;
     }
 
-    public void addAssign(String n,Assignment a){
-        for(Course c : classes){
-            if(c.name.equals(n))c.addAssign(a);
+    public void addAssign(String n, Assignment a) {
+        for (Course c : classes) {
+            if (c.name.equals(n)) {
+                c.addAssign(a);
+            }
         }
     }
 
-    public void addType(String n,Type a){
-        for(Course c : classes){
-            if(c.name.equals(n))c.addType(a);
+    public void addType(String n, Type a) {
+        for (Course c : classes) {
+            if (c.name.equals(n)) {
+                c.addType(a);
+            }
         }
     }
 
-    public void addAbsences(String a){
+    public void addAbsences(String a) {
         absString = a;
         String[] arr = a.split(" ");
-        for(int i=0;i<arr.length;i++){
+        for (int i = 0; i < arr.length; i++) {
             abs[i] = Integer.parseInt(arr[i].split("-")[1]);
         }
     }
 
-    public int[] getAbs() {
-        return abs;
-    }
-
-    public String getAbsString() {
-        return absString;
-    }
-
-    public String toString(){
-        String ret = "{"+name+"}";
+    public String toString() {
+        String ret = "{" + name + "}";
         ret += classes.size();
-        for(Course c : classes){
-            ret+=c.toString();
+        for (Course c : classes) {
+            ret += c.toString();
         }
-        ret += "{"+absString+"}"+"{"+lunch+"}";
+        ret += "{" + absString + "}"+"{"+lunch+"}";
         return ret;
     }
 
-    public static ClssPkg parse(String str){
+    public static ClssPkg parse(String str) {
         Scanner s = new Scanner(str);
         s.useDelimiter("\\}\\{|\\}|\\{");
         ClssPkg clss = new ClssPkg(s.next());
         int iter1 = s.nextInt();
-        for(int i=0;i<iter1;i++){
-            Course c = new Course(s.next(),s.next(),s.next(),s.next(),s.next(),s.nextInt(),s.next(),s.next(),s.next(),s.next(),s.next(),s.next(),s.next(),s.next(),s.next(),s.next());
+        for (int i = 0; i < iter1; i++) {
+            Course c = new Course(s.next(), s.next(), s.next(), s.next(), s.next(), s.nextInt(), s.next(), s.next(), s.next(), s.next(), s.next(), s.next(), s.next(), s.next(), s.next(), s.next());
             int iter2 = s.nextInt();
-            for(int j=0;j<iter2;j++){
-                Assignment a = new Assignment(s.next(),s.next(),s.next(),s.next(),s.next());
+            for (int j = 0; j < iter2; j++) {
+                Assignment a = new Assignment(s.next(), s.next(), s.next(), s.next(), s.next(), s.next());
                 c.addAssign(a);
             }
             iter2 = s.nextInt();
-            for(int j=0;j<iter2;j++){
-                Type a = new Type(s.next(),s.nextDouble(),s.nextDouble(),s.nextDouble());
+            for (int j = 0; j < iter2; j++) {
+                Type a = new Type(s.next(), s.nextDouble(), s.nextDouble(), s.nextDouble());
                 c.addType(a);
             }
             clss.classes.add(c);
         }
         clss.addAbsences(s.next());
         clss.lunch = s.next();
-        Log.e("hello",clss.lunch);
-
         return clss;
     }
-
     public static ClssPkg getFromServer(String user, String pass){
         String s = "";
         try {
@@ -123,6 +117,5 @@ public class ClssPkg implements Serializable{
         }
         return parse(s);
     }
-
 
 }
